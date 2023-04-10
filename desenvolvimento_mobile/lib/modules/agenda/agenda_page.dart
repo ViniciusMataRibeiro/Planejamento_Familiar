78a061ba50c3324d8a916468c6a9b0a0193739bf
Marcus Paulo <71295372+marcuscmd@users.noreply.github.com>
2023-04-10 12:56:03 -0300

    fazendo o drawer da page agenda
    estilizando page

--- desenvolvimento_mobile/lib/modules/agenda/agenda_page.dart
+++ desenvolvimento_mobile/lib/modules/agenda/agenda_page.dart
@@ -7,6 +7,74 @@ class AgendaPage extends GetView<AgendaController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      drawer: Drawer(
+        backgroundColor:const Color.fromARGB(190, 255, 115, 95),
+        child: ListView(
+          children: const [
+             SizedBox(height: 30,),
+             ListTile(
+              tileColor: Colors.redAccent,
+              iconColor: Colors.white,
+              leading: Icon(Icons.family_restroom_outlined,
+              size: 40),
+              title: Text('Planejamento Familiar',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              )),
+            ),
+            Divider(
+              thickness: 2,
+            ),
+             ListTile(
+              tileColor: Colors.redAccent,
+              iconColor: Colors.white,
+              leading: Icon(Icons.family_restroom_outlined,
+              size: 40),
+              title: Text('Quebrando o Tabu',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              )),
+            ),
+            Divider(
+              thickness: 2,
+            ),
+             ListTile(
+              tileColor: Colors.redAccent,
+              iconColor: Colors.white,
+              leading: Icon(Icons.family_restroom_outlined,
+              size: 40),
+              title: Text('Métodos Hormonais',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              )),
+            ),
+            Divider(
+              thickness: 2,
+            ),
+             ListTile(
+              tileColor: Colors.redAccent,
+              iconColor: Colors.white,
+              leading: Icon(Icons.family_restroom_outlined,
+              size: 40),
+              title: Text('Métodos não Hormanais',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              )),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text('Agenda'),
+      ),
+      body: const Center(
+        child: Text('Teste'),
+      ),
+    );
   }
 }
